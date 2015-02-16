1ec419c32e3b884f13a4981ae2e921bb0c7db85c
Schulung <cbokeloh@gmail.com>
2015-02-16 16:02:35 +0100

    waitnotify Uebung

---
+++ UebMultithreading/waitnotify/Waiter.java
@@ -0,0 +1,29 @@
+package UebMultithreading.waitnotify;
+
+import java.util.Date;
+
+public class Waiter implements Runnable{
+    
+    private Message msg;
+     
+    public Waiter(Message m){
+        this.msg=m;
+    }
+ 
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        synchronized (msg) {
+            try{
+                System.out.println(name+" waiting to get notified at time:"+new Date());
+                msg.wait();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(name+" waiter thread got notified at time:"+new Date());
+            //process the message now
+            System.out.println(name+" processed: "+msg.getMsg());
+        }
+    }
+ 
+}
