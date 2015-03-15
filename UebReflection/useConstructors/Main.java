c7cfea0f4f1fedc681d19d2dbde932f5f74b1413
Schulung <cbokeloh@gmail.com>
2015-03-15 16:23:43 +0100

    Uebungen reflection

--- UebReflection/useConstructors/Main.java
+++ UebReflection/useConstructors/Main.java
@@ -11,7 +11,6 @@ import UebReflection.listconstructors.Rectangle;
 
 public class Main {
 	  public static void main(String[] args) {
-	    // An array of Class objects with the correct types for the constructor
 	    Class[] parmTypes = {String.class, int.class, int.class, Color.class};
 	    // Get a reference to the constructor we want to use
 	    Class rectangleClassObject = Rectangle.class;
@@ -19,24 +18,24 @@ public class Main {
 	    try {
 	      constructor = rectangleClassObject.getConstructor(parmTypes);
 	    } catch (NoSuchMethodException ex) {
-	      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	    	ex.printStackTrace();
 	    } catch (SecurityException ex) {
-	      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	      ex.printStackTrace();
 	    }
 	    // An array with object values we want to use
-	     Object[] parms = {"Room A", 7, 10, Color.red};
+	     Object[] parms = {"Raum A", 7, 10, Color.red};
 	    // create the Object with the newInstance() method
 	    try {
 	      Rectangle p = (Rectangle) constructor.newInstance(parms);
 	      System.out.println(p);
 	    } catch (InstantiationException ex) {
-	      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	    	ex.printStackTrace();
 	    } catch (IllegalAccessException ex) {
-	      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	    	ex.printStackTrace();
 	    } catch (IllegalArgumentException ex) {
-	      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	    	ex.printStackTrace();
 	    } catch (InvocationTargetException ex) {
-	      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+	    	ex.printStackTrace();
 	    }
 	  }
 
